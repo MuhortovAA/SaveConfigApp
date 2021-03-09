138be129550ffb4d0d2913ffe14a55d0c660b175
Alex M <xbestyan@hotmail.com>
2021-03-09 20:22:48 +0300

    add project

---
+++ SaveConfigApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveConfigApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            AddUpdateAppSettings("Setting1", "May 8, 2014");
+            var result = GetSetting("Setting2");
+			Console.WriteLine(result);
+
+        }
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+        private static string GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
